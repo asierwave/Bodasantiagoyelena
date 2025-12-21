7f5dc885dfca61c8e08ecca35eda716f2dce2dac
santiagosantamejia-bit <santiagosantamejia@gmail.com>
2025-12-21 02:16:57 +0100

    Update App.tsx
    nuevo cambio en la pantalla

--- src/App.tsx
+++ src/App.tsx
@@ -630,8 +630,8 @@ function CeremonySection() {
       >
         <iframe
           src={`https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=${encodeURIComponent(GOOGLE_MAPS_ADDRESS)}&zoom=15`}
-          width="100%"
-          height="100%"
+          width="50%"
+          height="50%"
           style={{ border: 0 }}
           allowFullScreen
           loading="lazy"
