0947fe883d8c2d7ad9f71490e22fb41a0ec56731
santiagosantamejia-bit <santiagosantamejia@gmail.com>
2025-12-23 00:30:05 +0100

    mirando
    ahora ?

--- src/App.tsx
+++ src/App.tsx
@@ -637,10 +637,6 @@ function CeremonySection() {
   );
 }
 
-import { useRef } from "react";
-import { motion, useScroll, useTransform } from "framer-motion";
-import imgFrame5 from "./assets/banquete.jpeg";
-
 const CASA_BURGOS_URL =
   "https://maps.google.com/?cid=6180376183828684675";
 
