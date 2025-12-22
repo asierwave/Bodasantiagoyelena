8037f772c33960221d0a6e6ca4aa8ec51d596eed
santiagosantamejia-bit <santiagosantamejia@gmail.com>
2025-12-23 00:13:22 +0100

    url
    casa burgor

--- src/App.tsx
+++ src/App.tsx
@@ -637,7 +637,7 @@ function CeremonySection() {
   );
 }
 
-const CASA_BURGOS_URL = "https://maps.google.com/?q=Casa+de+Burgos+Madrid";
+const CASA_BURGOS_URL = "https://maps.google.com/?cid=6180376183828684675&g_mp=Cidnb29nbGUubWFwcy5wbGFjZXMudjEuUGxhY2VzLlNlYXJjaFRleHQ";";
 function BanqueteSection() {
   const ref = useRef<HTMLDivElement>(null);
 
